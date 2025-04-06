662bbec3e62974e2f00922470c68a4fe70c31228
SaswatiGhosh <saswatititli99@gmail.com>
2025-04-06 14:24:21 +0530

    second commit

--- chaiadmin/tweet/views.py
+++ chaiadmin/tweet/views.py
@@ -11,9 +11,9 @@ def index(request):
     return render(request, 'index.html')
 
 def tweet_list(request):
-    print(request)
+    # print(request)
     tweets=Tweet.objects.all().order_by('-created_at')
-    print(tweets)
+    # print(tweets)
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
 def tweet_create(request):
@@ -24,9 +24,9 @@ def tweet_create(request):
             tweet.user = request.user
             tweet.save()
             return redirect('tweet_list')
-        else:
-            form=TweetForm()
-        return render(request, 'tweet_form.html', {'form': form})
+    else:
+        form=TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
 
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id,user= request.user)
@@ -37,9 +37,9 @@ def tweet_edit(request, tweet_id):
             tweet.user = request.user
             tweet.save()
             return redirect('tweet_list')
-        else:
-            form = TweetForm(instance=tweet)
-        return render(request , 'tweet_form.html' , {'form': form})
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request , 'tweet_form.html' , {'form': form})
         
 
 def tweet_delete(request, tweet_id):
@@ -47,4 +47,5 @@ def tweet_delete(request, tweet_id):
     if request.method == 'POST':
         tweet.delete()
         return redirect('tweet_list')
+
     return render(request , 'tweet_confirm_delete.html' , {'tweet': tweet} )
